dbf7febcde944e79cc852dedd17d26431980cd4e
Luis Lindarte <llindarte@coruniamericana.edu.co>
2024-11-07 11:11:34 -0500

    feat: quitar llibreria no usada

--- src/components/PaginaRecuperarContrasena.jsx
+++ src/components/PaginaRecuperarContrasena.jsx
@@ -1,6 +1,5 @@
 import {React} from 'react';
 import Header from './Header';
-import BreadCrumbArea from './BreadCrumbArea';
 import FormularioRecuperarContrasena from './FormularioRecuperarContrasena';
 import FooterArea from './FooterArea';
 
--- src/components/TarjetaCategoriaAdmin.jsx
+++ src/components/TarjetaCategoriaAdmin.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { AuthContext } from '../AuthContext';
 
 function TarjetaCategoriaAdmin(
