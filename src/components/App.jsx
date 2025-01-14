5f2a83c359b3048b4adde7651c5a819fec43d240
Kevin Mariotis <mariotiskevin@gmail.com>
2025-01-14 17:40:04 -0500

    implementacion del Custom cursor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ import { Provider } from "react-redux";
 import store from "../redux/Store";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
-import CustomCursor from "./CustomCursor/CustomCursor";
+import AnimatedCursor from "react-animated-cursor";
 
 function App() {
   useEffect(() => {
@@ -20,7 +20,21 @@ function App() {
       <Provider store={store}>
         <PersistGate persistor={persistor}>
           <AuthProvider>
-            <CustomCursor />
+            <AnimatedCursor
+              innerSize={25}
+              outerSize={50}
+              innerScale={1}
+              outerScale={2}
+              outerAlpha={0}
+              hasBlendMode={true}
+              innerStyle={{
+                backgroundColor: "var(--Lavander)",
+                border: "1px solid white",
+              }}
+              outerStyle={{
+                border: "2px solid var(--Azul-petroleo)",
+              }}
+            />
             <Rutas />
           </AuthProvider>
         </PersistGate>
