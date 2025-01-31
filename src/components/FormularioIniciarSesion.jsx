b436e9de78138245473632de65887390aa9789dc
Kevin Mariotis <mariotiskevin@gmail.com>
2025-01-31 09:32:30 -0500

    Modificacion en el estilo del bienvenidos

--- src/components/FormularioIniciarSesion.jsx
+++ src/components/FormularioIniciarSesion.jsx
@@ -245,7 +245,7 @@ function FormularioIniciarSesion() {
           <div className="col-lg-6 mx-auto">
             <div className="card card-item card-round">
               <div className="card-body p-0">
-                <h3 className="card-title text-center fs-40 lh-35 pb-2 mb-4">
+                <h3 className="card-title text-center fs-40 lh-35 pb-2 mb-4" style={{color: "var(--Lavander)"}}>
                   ¡Bienvenido a Glomind!
                 </h3>
                 <h3 className="card-title text-start fs-24 lh-35 pb-2">
--- src/components/FormularioRegistarse.jsx
+++ src/components/FormularioRegistarse.jsx
@@ -1,4 +1,4 @@
-import{ useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import ReCAPTCHA from "react-google-recaptcha";
 import { AuthContext } from "../AuthContext";
@@ -255,6 +255,12 @@ function FormularioRegistrarse() {
               <div className="col-lg-7 mx-auto">
                 <div className="card card-item card-round">
                   <div className="card-body p-0">
+                    <h3
+                      className="card-title text-center fs-40 lh-35 pb-2 mb-4"
+                      style={{ color: "var(--Lavander)" }}
+                    >
+                      ¡Bienvenido a Glomind!
+                    </h3>
                     <h3 className="card-title text-start fs-24 lh-35 pb-4">
                       Crea una cuenta
                     </h3>
